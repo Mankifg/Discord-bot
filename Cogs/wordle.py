3aa5c4986ac8e960d7d6182419ac1df5bd66e5f3
Mankifg <83959255+Mankifg@users.noreply.github.com>
2022-08-19 11:47:56 +0200

    wordle

--- Cogs/wordle.py
+++ Cogs/wordle.py
@@ -23,28 +23,26 @@ class wordleCog(commands.Cog, name="ping command"):
 
         p = ctx.author.name
         word = "abcde"
-        output = ["_ _ _ _ _ ",""
-                "_ _ _ _ _ ", ""
-                "_ _ _ _ _ ", ""
-                "_ _ _ _ _ ", ""
-                "_ _ _ _ _ ",""
-                "_ _ _ _ _ ",""]
+        output = ["_ _ _ _ _ ",
+                "_ _ _ _ _ ", 
+                "_ _ _ _ _ ", 
+                "_ _ _ _ _ ", 
+                "_ _ _ _ _ ",
+                "_ _ _ _ _ ",]
 
         round = 1
         player = ''
-        saved = '' 
-        a = 0 
         while True:
-            
-
             if round == 6:
                 await ctx.send(f"You lose, the word was {word}.")
                 break
 
 
             display = ''
-            for x in range(len(output)):
-                display = display + " " + output[x] + "\n"
+            for z in range(len(output)):
+                for char in output[z]:
+                    display += char
+                display += '\n'
 
             display = f"```{display}```"
 
@@ -66,28 +64,27 @@ class wordleCog(commands.Cog, name="ping command"):
             if not len(player) == 5:
                 await ctx.send(f"{player} is not a valid word.")
                 continue
-            colors = ["","","","",""]
+            colors =['','','','','']
             out = ''
             ponavljanje = {'a':0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0, 'g':0, 'h':0, 'i':0, 'j':0, 'k':0, 'l':0, 'm':0, 'n':0, 'o':0, 'p':0, 'q':0, 'r':0, 's':0, 't':0, 'u':0, 'v':0, 'w':0, 'x':0, 'y':0, 'z':0}
             if len(player) == 5:
                 for i in range(len(player)):
                     if player[i] == word[i]:
-                        colors[i] = 'g'
-                        out[i] = green(player[i]) + " "
+                        colors[i] = 'g' 
+                        out[i] = player[i] + " "
                         ponavljanje[player[i]] += 1
 
+     
                 for i in range(len(player)):
                     if player[i] in word and ponavljanje[player[i]] < word.count(player[i]):
                         colors[i] = 'y'
-                        out[i] = yellow(player[i]) + " "
+                        out[i] = player[i] + " "
                     elif player[i] != word[i]:
                         colors[i] = 'r'
                         out[i] = player[i] + " "
-
-            
+    
+                
                 save = ''
-
-
                 for i in range(len(colors)):
                     if colors[i] == "g":
                         save = save + green + " "
@@ -97,9 +94,6 @@ class wordleCog(commands.Cog, name="ping command"):
                         save = save + gray + " "
 
                 await ctx.send(save)
-                a = a + 2
-            else:
-                pass
 
                         
 def setup(bot: commands.Bot):

ca696aa1863de7d306fbdd5e5446d43e6b4efae1
Mankifg <83959255+Mankifg@users.noreply.github.com>
2023-03-03 15:14:38 +0100

    bank.json on start | temp solution

--- startup.py
+++ startup.py
@@ -2,3 +2,9 @@ import os
 
 if not os.path.exists("Photos"):
     os.mkdir("Photos")
+
+
+if not os.path.isfile('data/bank.json'):
+    #open("data/bank.json", "w").close()
+    with open('data/bank.json',"w") as f:
+        f.write("{\n}")

709aa17819a0db9836bd2793c025d9486d268905
Mankifg <83959255+Mankifg@users.noreply.github.com>
2023-04-23 16:58:44 +0200

    fixed apod

--- Cogs/01_api/01_apod.py
+++ Cogs/01_api/01_apod.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import os
 import json
 import requests
@@ -7,51 +7,117 @@ from dotenv import load_dotenv, find_dotenv
 import random
 from functions import get_color
 import urllib.request
-
+import asyncio
+from datetime import datetime
 
 website = "https://api.nasa.gov/planetary/apod"
+path_to_img = "./data/Photos/apod.png"
 
 load_dotenv(find_dotenv())
 key = os.getenv("NASA")
 
-class ApodCog(commands.Cog, name="ping command"):
-    def __init__(self, bot: commands.bot):
-        self.bot = bot
+path = "data/settings.json"
 
-    @commands.command(name="apod", 
-    usage=" for Astronaut Picture of The day", 
-    description="Astronaut Picture of The day by Nasa", 
-    alias=['APOD', 'apod']
-    )
+def getFile():
+    with open(path,"r") as f:
+        return json.load(f)
+    
+def saveids(save):
+    with open(path,"w") as f: 
+        json.dump(save,f) 
 
-    @commands.cooldown(1, 2, commands.BucketType.member)
-    async def apod(self, ctx):
-        resp = requests.get(f"{website}?api_key={key}").json()
+def getTime():
+    file = getFile()
+    return file["apod"]["time"]
 
-        try:
-            author = resp["copyright"]
-        except:
-            author = ''
+class ApodCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot  
+        self.send_message.start()
+    @tasks.loop(seconds=59)
+    async def send_message(self):
+        if datetime.now().strftime("%H:%M") == getTime():
+            channels = getFile()["apod"]["ids"]
+            resp = requests.get(f"{website}?api_key={key}").json()
+            author = resp.get("copyright", "n/a")
+            date = resp.get("date","1.1.1970")
+            explain = resp.get("explanation","n/a")
+            pic_url = resp.get("url","https://i.redd.it/8w81b1pwk0d21.jpg")
+            title = resp.get("title","n/a")
+            explain = explain[0:200] + "..." if len(explain) > 200 else explain
             
-        date = resp["date"]
-        explain = resp["explanation"]
-        pic_url = resp["url"]
-        title = resp["title"]
+            urllib.request.urlretrieve(pic_url, path_to_img)
 
-        try:
-            explain = explain[0:200] + "..."
-        except:
-            explain = explain
+            q = discord.Embed(title="Astronomy Picture of the Day", description='', color=get_color(path_to_img))
+            q.add_field(name=f"Title: {title}", value=f"Author: {author}", inline=True)
+            q.set_image(url=pic_url)
+            q.add_field(name=f"Date: {date}", value=f'Desc: {explain}', inline=False)
+            q.set_footer(text="Using Nasa Api")
 
-        urllib.request.urlretrieve(pic_url, "./Photos/apod.png")
+            for id in channels:
+                ch = self.bot.get_channel(id)
+                await ch.send(embed=q)
+            await asyncio.sleep(23*60*60) # number of second in 23 hours, i think it shoud be good but idk
 
-        q = discord.Embed(title="Astronomy Picture of the Day", description='', color=get_color("./Photos/apod.png"))
-        q.add_field(name="Title: " + title , value="Author: " + author, inline=True)
-        q.set_image(url=pic_url)
-        q.add_field(name="Date: " + date,value='Desc: ' + explain , inline=False)
-        q.set_footer(text="Using Nasa Api")
+
+    @send_message.before_loop
+    async def before_send_message(self):
+        await self.bot.wait_until_ready()
+
+    @commands.command(name="apod", usage="", description="apod")
+    @commands.cooldown(1, 2, commands.BucketType.member)
+    async def apod(self, ctx, mode=""):
+        mode = mode.lower()
+        if mode in ["show", "s"]:
+            #! If user wants the image
+            resp = requests.get(f"{website}?api_key={key}").json()
+            author = resp.get("copyright", "n/a")
+            date = resp.get("date","1.1.1970")
+            explain = resp.get("explanation","n/a")
+            pic_url = resp.get("url","https://i.redd.it/8w81b1pwk0d21.jpg")
+            title = resp.get("title","n/a")
+            urllib.request.urlretrieve(pic_url, path_to_img)
+
+            explain = explain[0:200] + "..." if len(explain) > 200 else explain
+
+            q = discord.Embed(title="Astronomy Picture of the Day", description='', color=get_color(path_to_img))
+            q.add_field(name=f"Title: {title}", value=f"Author: {author}", inline=True)
+            q.set_image(url=pic_url)
+            q.add_field(name=f"Date: {date}", value=f'Desc: {explain}', inline=False)
+            q.set_footer(text="Using Nasa Api")
+
+            await ctx.send("If you want daily pleas type m!apod add")
+            
+        elif mode in ["add", "a"]:
+            file = getFile()
+            id = ctx.channel.id
+
+            if not id in file["apod"]["ids"]:
+                file["apod"]["ids"].append(ctx.channel.id)
+                q = discord.Embed(title=f"Added channel with id {id} to daily apod",color=discord.Color.green())
+            else:
+                q = discord.Embed(title=f"Channel with id {id} already in list",color=discord.Color.yellow())
+            saveids(file)
+
+        elif mode in ["remove", "r", "rmv", "rem"]:
+            file = getFile()
+            id = ctx.channel.id
+
+            if id in file["apod"]["ids"]:
+                file["apod"]["ids"].remove(ctx.channel.id)
+                q = discord.Embed(title=f"Removed channel with id {id} from daily apod",color=discord.Color.green())
+            else:
+                q = discord.Embed(title=f"Channel with id: **{id}** isn't in daily apod",color=discord.Color.yellow())
+            saveids(file)
+    
+        else:
+            q = discord.Embed(title="Error", description="Please use one of the following commands",
+                              color=discord.Colour.red())
+            q.add_field(name="m!apod show",value="Shows daily apod")
+            q.add_field(name="m!apod add",value="Add a channel to list of channels to send daily apod")
+            q.add_field(name="m!apod remove",value="Remove a channel from list of daily apod")
+
         await ctx.send(embed=q)
 
-def setup(bot: commands.Bot):
-   
-    
+def setup(bot):
+    bot.add_cog(ApodCog(bot))
--- startup.py
+++ startup.py
@@ -1,10 +1,18 @@
 import os
+import json
 
 if not os.path.exists("Photos"):
     os.mkdir("Photos")
-
+if not os.path.exists("data/Photos"):
+    os.mkdir("data/Photos")
 
 if not os.path.isfile('data/bank.json'):
     #open("data/bank.json", "w").close()
     with open('data/bank.json',"w") as f:
         f.write("{\n}")
+        f.write("{\n}")
+
+if not os.path.isfile('data/settings.json'):
+    with open('data/settings.json',"w") as f:
+        with open("data/global_settings.json","r") as g:
+            json.dump(json.load(g), f)
